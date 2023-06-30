557b5cae13e7cf6e4c3b93803fbf945cdffc40ee
Mathias Lucena <103156188+SadBeatrice@users.noreply.github.com>
2023-06-30 08:06:33 -0300

    Add files via upload

---
+++ Atividades/IFA/IFA/main.js
@@ -0,0 +1,361 @@
+data = [
+    {doctor: 'Jonas', 
+        local:{description: '3º sáb. às 17:00h', day: 6, Semana: 3},
+    },
+    {doctor: 'Natan', 
+        gem:{description: '4º sex. às 20:00h', day: 5, Semana: 4},
+        local:{description: '2º sáb. às 17:00h', day: 6, Semana: 2},
+    },
+    {doctor: 'Mário', 
+        local:{description: '1º sáb. às 17:00h', day: 6, Semana: 1}
+    },
+    {doctor: 'Maria', 
+        gem:{description: 'último sáb. às 14:30h', day: 6, Semana: -1},
+        local:{description: '1º dom. às 16:00h', day: 0, Semana: 1}
+    },
+	{doctor: 'José', 
+        gem:{description: '2ª sex. às 19:30h', day: 5, Semana: 2},
+        local:{description: '4º sáb. às 19:30h', day: 6, Semana: 4},
+    },
+	{doctor: 'Caio',  
+        gem:{description: 'todo o dom. às 09:10h', day: 0, Semana: 0},
+        local:{description: '1º sáb. às 17:00h', day: 6, Semana: 1}
+    },
+    {doctor: 'Michele', 
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'Renata', 
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Natasha', 
+        local:{description: '2º sáb. às 16h30h', day: 6, Semana: 2}
+    },
+    {doctor: 'Natália', 
+        gem:{description: '1º sáb. às 15:30h', day: 6, Semana: 1},
+        local:{description: '1º dom. às 16:00h', day: 0, Semana: 1}
+    },
+    {doctor: 'Luana', 
+        local:{description: '3º sáb. às 17:00h', day: 6, Semana: 3}
+    },
+    {doctor: 'Patrícia', 
+        local:{description: '4º sáb. às 16:00h', day: 6, Semana: 4}
+    },
+    {doctor: 'Camila', 
+        gem:{description: 'última qui. às 19:30h', day: 4, Semana: -1},
+        local:{description: 'último sáb. às 19:30h', day: 6, Semana: -1}
+    },
+    {doctor: 'Marta', 
+        gem:{description: '2º sáb. às 17:00h', day: 6, Semana: 2},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Márcia', 
+        gem:{description: '2º sáb. às 15:00h', day: 6, Semana: 2},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Johnata', 
+        local:{description: '2º sáb. às 19:30h', day: 6, Semana: 2}
+    },
+    {doctor: 'Maiara', 
+        gem:{description: '3º sáb. às 16:00h', day: 6, Semana: 3},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Renato', 
+        gem:{description: '3ª ter. às 20:00h', day: 2, Semana: 3},
+        local:{description: '3º sáb. às 19:30h', day: 6, Semana: 3}
+    },
+    {doctor: 'Paulo', 
+        gem:{description: '3º sáb. às 14:30h', day: 6, Semana: 3},
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'Taís', 
+        gem:{description: '3ª sex. às 20:00h', day: 5, Semana: 3},
+        local:{description: '1º dom. às 16:00h', day: 0, Semana: 1}
+    },
+    {doctor: 'Carlos', 
+        gem:{description: '3ª sex. às 19:30h', day: 5, Semana: 3},
+        local:{description: '1º sáb. às 17:00h', day: 6, Semana: 1}
+    },
+    {doctor: 'Tamiris', 
+        gem:{description: '1º sáb. às 10:30h', day: 6, Semana: 1},
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'João', 
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'Lucas', 
+        local:{description: '3º sáb. às 19:30h', day: 6, Semana: 3}
+    },
+    {doctor: 'Marcos', 
+        gem:{description: 'última qui. às 20:00h', day: 4, Semana: -1},
+        local:{description: '4º sex. às 20:00h', day: 5, Semana: 4}
+    },
+    {doctor: 'Mateus', 
+        gem:{description: 'última qua. às 19:30h', day: 3, Semana: -1},
+        local:{description: '1º sáb. às 16:30h', day: 6, Semana: 1},
+    },
+    {doctor: 'Jaqueline', 
+        local:{description: 'Não tem ensaio', day: 50, Semana: -2},
+    },
+    {doctor: 'Sarah', 
+        gem:{description: '1º sáb. às 15:00h', day: 6, Semana: 1},
+        local:{description: '3º qua. às 19:30h', day: 4, Semana: 3},
+    },
+    {doctor: 'Daniel', 
+        local:{description: '4º sáb. às 17:00h', day: 6, Semana: 4},
+    },
+    {doctor: 'Márcio', 
+        local:{description: '1º sex. às 19:30h', day: 5, Semana: 1},
+    },
+    {doctor: 'Danilo', 
+        local:{description: '3º sáb. às 19:30h', day: 6, Semana: 3}
+    },
+    {doctor: 'Melissa', 
+        gem:{description: '3ª sex. às 19:30h', day: 5, Semana: 3},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Davi', 
+        local:{description: '1º dom. às 16:00h', day: 0, Semana: 1}
+    },
+    {doctor: 'Andréia', 
+        local:{description: '1º sex. às 19:30h', day: 5, Semana: 1}
+    },
+    {doctor: 'Amanda', 
+        gem:{description: 'última qua. às 20:00h', day: 3, Semana: -1},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Miguel', 
+        gem:{description: '1ª seg. às 19:30h', day: 1, Semana: 1},
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Ana', 
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Anderson', 
+        gem:{description: 'todo o sáb. às 16:00h', day: 6, Semana: 0},
+        local:{description: 'último sáb. às 17:00h', day: 6, Semana: -1}
+    },
+    {doctor: 'Jônatas', 
+        gem:{description: 'última ter. às 19:30h', day: 2, Semana: -1},
+        local:{description: '2º sáb. às 19:30h', day: 6, Semana: 2}
+    },
+    {doctor: 'Carolina', 
+        gem:{description: 'último dom. às 09:00h', day: 0, Semana: -1},
+        local:{description: '1º dom. às 16h30h', day: 0, Semana: 1}
+    },
+    {doctor: 'Ricardo', 
+        gem:{description: 'toda a seg. às 20:00h', day: 1, Semana: 0},
+        local:{description: '2º sáb. às 17:00h', day: 6, Semana: 2}
+    },
+    {doctor: 'Lúcia', 
+        local:{description: '4º dom. às 16:00h', day: 0, Semana: 4}
+    },
+    {doctor: 'Cláudia', 
+        gem:{description: '3ª seg. às 19:30h', day: 1, Semana: 3},
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'Antônio', 
+        local:{description: 'último dom. às 16:00h', day: 0, Semana: -1}
+    },
+    {doctor: 'Sérgio', 
+        local:{description: 'último sáb. às 17:00h', day: 6, Semana: -1}
+    },
+    {doctor: 'Samuel', 
+        gem:{description: 'última seg. às 19:30h', day: 1, Semana: -1},
+        local:{description: '2º sex. às 19:30h', day: 5, Semana: 2}
+    },
+    {doctor: 'Mônica', 
+        gem:{description: 'toda a seg. às 20:30h', day: 1, Semana: 0},
+        local:{description: '1º sáb. às 19:30h', day: 6, Semana: 1}
+    },
+]
+
+function converterDataParaString(dataparaconversão) {
+   
+    // Extrai o dia, mês e ano da data calculada
+    var dia = dataparaconversão.getDate();
+    var mes = dataparaconversão.getMonth() + 1;
+    var ano = dataparaconversão.getFullYear();
+    
+    // Formata os valores para dois dígitos
+    dia = dia < 10 ? '0' + dia : dia;
+    mes = mes < 10 ? '0' + mes : mes;
+    
+    // Cria a string da data no formato "DD/MM/AAAA"
+    var dataString = dia + '/' + mes + '/' + ano;
+    
+    // Retorna a data formatada
+    return dataString;
+}
+
+function verificarSeJaPassou(data) {
+    let dataAtual = new Date();
+
+    if (data < dataAtual) {
+        return true; // A data já passou
+    } else {
+        return false; // A data ainda está por vir
+    }
+}
+
+function ordenaçãoDiaMes(day, ordenação){
+    let dataAtual = new Date();
+    let mesAtual = dataAtual.getMonth();
+    let contadorDeSemanas = 0;
+    dataAtual.setDate(1);
+    while (dataAtual.getMonth() === mesAtual) {
+        if (dataAtual.getDay() === day) {
+            contadorDeSemanas++;
+            if (contadorDeSemanas === ordenação) {
+                resposta = new Date(dataAtual);
+            }
+        }
+        dataAtual.setDate(dataAtual.getDate() + 1);
+    }
+    if (verificarSeJaPassou(resposta)) {
+        let dataAtual = new Date();
+        let mesAtual = dataAtual.getMonth();
+        let anoAtual = dataAtual.getFullYear();
+        let contadorDeSemanas = 0;
+        dataAtual.setDate(1);
+        mesAtual++
+        dataAtual.setFullYear(anoAtual, mesAtual, 1);
+    
+        while (dataAtual.getMonth() === mesAtual) {
+            if (dataAtual.getDay() === day) {
+                contadorDeSemanas++;
+                if (contadorDeSemanas === ordenação) {
+                  resposta = new Date(dataAtual);
+                  return (converterDataParaString(resposta)); 
+                }
+              }
+              dataAtual.setDate(dataAtual.getDate() + 1);
+            }
+        return null;
+        }
+    else {
+        return (converterDataParaString(possivelResposta));
+    }
+}
+
+function calcularProximoDiaSemana(day) {
+    var dataAtual = new Date();
+    var diaSemanaAtual = dataAtual.getDay();
+  
+    var diasParaAdicionar = (day - diaSemanaAtual + 7) % 7; // Calcula a diferença em dias até o próximo dia da semana
+  
+    var proximaData = new Date(dataAtual.getFullYear(), dataAtual.getMonth(), dataAtual.getDate() + diasParaAdicionar);
+  
+    return converterDataParaString(proximaData);
+}
+
+function calcularÚltimoDiaSemana(day) {
+    var dataAtual = new Date();
+    var ultimoDiaMes = new Date(dataAtual.getFullYear(), dataAtual.getMonth() + 1, 0);
+  
+    // Ajusta o objeto Date para o último dia da semana correspondente
+    ultimoDiaMes.setDate(ultimoDiaMes.getDate() - (ultimoDiaMes.getDay() + (7 - day)) % 7);
+    
+    if (verificarSeJaPassou(ultimoDiaMes)) {
+        var dataAtual = new Date();
+        var ultimoDiaMes = new Date(dataAtual.getFullYear(), dataAtual.getMonth() + 2, 0);
+        ultimoDiaMes.setDate(ultimoDiaMes.getDate() - (ultimoDiaMes.getDay() + (7 - day)) % 7);
+        return converterDataParaString(ultimoDiaMes);
+    }
+    else{
+        return converterDataParaString(ultimoDiaMes);
+    }
+}
+
+function buscarResposta(day, ordenação){
+    
+    if (ordenação === -1){
+        var resposta = calcularÚltimoDiaSemana(day)
+        return resposta;
+    }
+    else if(ordenação === 0) {
+        var resposta = calcularProximoDiaSemana(day)
+        return resposta;
+    }
+    else if(ordenação === -2) {
+        return null;
+    }
+    else {
+        var resposta = ordenaçãoDiaMes(day, ordenação)
+        return resposta;
+    }
+}
+
+data.forEach(element => {
+    if (element.hasOwnProperty('gem')) {
+        
+        var respostaFinal = buscarResposta(element.gem.day, element.gem.Semana);
+        let doctor = 
+        `<tr>
+            <td>
+                ${element?.doctor}
+            </td>
+            <td>
+                ${element.gem?.description}
+            </td>
+            <td>
+                ${respostaFinal}
+            </td>
+        </tr>`
+
+        $("#tb_gem").append(doctor)
+    }
+    
+
+    if(element.hasOwnProperty('local')){
+
+        var respostaFinal = buscarResposta(element.local.day, element.local.Semana);
+        let doctor = 
+        `<tr>
+            <td>
+                ${element.doctor}
+            </td>
+            <td>
+                ${element.local?.description}
+            </td>
+            <td>
+                ${respostaFinal}
+            </td>
+        </tr>`
+
+        $("#tb_local").append(doctor)
+    }
+});
+
+$(document).ready(function() {
+    $('#data_gem').DataTable( {
+        rowReorder: {
+            selector: 'td:nth-child(2)'
+        },
+        responsive: true,
+        paging: false,
+        language: {
+            search: "Pesquisar:",
+            lengthMenu: "Show _MENU_ entries",
+            info: "Mostrando _TOTAL_ médico(s)",
+            infoEmpty: "Mostrando 0",
+            infoFiltered: "(Filtradas de _MAX_)",
+            zeroRecords: "Nenhum médico encontrado",
+        },
+    } );
+
+    $('#data_local').DataTable( {
+        rowReorder: {
+            selector: 'td:nth-child(2)'
+        },
+        responsive: true,
+        paging: false,
+        language: {
+            search: "Pesquisar:",
+            lengthMenu: "Show _MENU_ entries",
+            info: "Mostrando _TOTAL_ médico(s)",
+            infoEmpty: "Mostrando 0",
+            infoFiltered: "(Filtradas de _MAX_)",
+            zeroRecords: "Nenhum médico encontrado",
+        },
+    } );
+} );
